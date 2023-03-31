c8ea177c6e1beb2b2fbf29e6ff151363cec6f2dc
RayraHy <rayra.hyasmymb@gmail.com>
2023-03-30 22:50:29 -0300

    plus++

---
+++ condicinais-e-lista/exercicios/js/app.js
@@ -0,0 +1,27 @@
+new Vue({
+	el: '#desafio',
+	data: {
+		fra: false,
+		array: ['Pedro', 'Bia', 'Ana', 'Rebeca'],
+		livros:[
+		{
+			titulo: 'O Senhor dos Anéis',
+			autor: 'J.R.R. Tolkiens',
+			volume: '3'
+		},
+		{
+			titulo: 'InuYasha',
+			autor: 'Rumiko Takahashi',
+			volume: '5'
+		},
+	],
+		alunos: 
+			{
+				nome: 'Maria',
+				idade: 10,
+				notas: [7.67, 8.33, 6.98, 9.21]
+			},
+			
+				
+	}
+});
