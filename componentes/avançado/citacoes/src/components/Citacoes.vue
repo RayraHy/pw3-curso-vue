1791baabac0a1f10dff59cfd82420d5bcc73895c
Rayra <rayra.hyasmymb@gmail.com>
2023-05-04 22:20:20 -0300

    add-componente-desafio

---
+++ componentes/avançado/citacoes/src/components/Citacoes.vue
@@ -0,0 +1,69 @@
+<template>
+    <div class="citacoes">
+        <span>
+            <button @click="numero--">&lt;</button>
+            <button @click="numero++">&gt;</button>
+        </span>
+        <!-- <div>{{ citacoes[indice].texto }}</div>
+        <Citacao>
+            <h2>Texto</h2>
+        </Citacao>
+        <Citacao></Citacao>
+        <Citacao>
+            <h1>{{ citacoes[indice].autor }}</h1>
+            <P>{{ citacoes[indice].texto }}</P>
+            <h6>{{ citacoes[indice].fonte }}</h6>
+        </Citacao>-->
+        <Citacao>
+            <h1 slot="autor">{{ citacoes[indice].autor }}</h1>
+            <P slot="texto">{{ citacoes[indice].texto }}</P>
+            <h6 slot="fonte">{{ citacoes[indice].fonte }}</h6>
+        </Citacao>
+    </div>
+</template>
+
+<script>
+import Citacao from '../components/Citacao.vue'
+
+export default {
+  components: { 
+    Citacao 
+},
+    data() {
+        return {
+            numero: 0,
+            citacoes: [{
+                fonte: 'Jornal do Empreendedor',
+                texto: 'Lembre-se sempre que você é absolutamente único. Assim como todos os outros.',
+                autor: 'Margaret Mead'
+            }, { 
+                fonte: 'Frases de Mãe',
+                texto: 'Isso não é um quarto, é um chiqueiro.',
+                autor: 'Roberta'
+            }, {
+                fonte: 'Frases de Pai',
+                texto: 'Vou contar até 3! 1, 2, 2...',
+                autor: 'Gustavo'
+            },
+            {
+                fonte: 'Preguisozo',
+                texto: 'Preguisa',
+                autor: 'Desmond'
+            }]
+        }
+    },
+    computed: {
+        indice() {
+            return Math.abs(this.numero % this.citacoes.length)
+        }
+    }
+}
+</script>
+
+<style scoped>
+    .citacoes {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+    }
+</style>
--- componentes/componente-desafio/src/App.vue
+++ componentes/componente-desafio/src/App.vue
@@ -1,67 +1,39 @@
 <template>
   <v-app>
     <v-navigation-drawer app>
-      <v-toolbar flat>
-        <v-list>
-          <v-list-tile>
-            <v-list-tile-title class="title">
-                Menu
-            </v-list-tile-title>
-          </v-list-tile>
-        </v-list>
-      </v-toolbar>
+      <Toolbar></Toolbar>
 
       <v-divider></v-divider>
 
-      <v-list dense class="pt-0">
-        <v-list-item v-for="item in itensMenu" :key="item.titulo">
-          <v-list-item-icon>
-            <v-icon>{{ item.icon }}</v-icon>
-          </v-list-item-icon>
-          <v-list-item-content>
-            <v-list-item-title>{{ item.titulo }}</v-list-item-title>
-          </v-list-item-content>
-        </v-list-item>
-      </v-list>
+      <List></List>
     </v-navigation-drawer>
 
-    <v-app-bar app>
-        <v-toolbar-title class="headline text-uppercase">
-				<span>Desafio</span>
-				<span class="font-weight-light">Componente</span>
-        </v-toolbar-title>
-    </v-app-bar>
+    <Bar></Bar>
 
-    <v-content>
-      <v-carousel>
-        <v-carousel-item v-for="(item,i) in fotos" :key="i" :src="item.src">
-          
-        </v-carousel-item>
-      </v-carousel>
-    </v-content>
+    <Carrosel></Carrosel>
 
-    <v-footer class="pa-3" app>
-      <v-spacer></v-spacer>
-      <div>Curso Vue &copy; {{ new Date().getFullYear() }}</div>
-    </v-footer>
+    <Footer></Footer>
   </v-app>
 </template>
 
 <script>
+import Carrosel from './componentes/Carrosel.vue'
+import Footer from '../src/componentes/Footer.vue'
+import Bar from '../src/componentes/Bar.vue'
+import List from '../src/componentes/List.vue'
+import Toolbar from '../src/componentes/Toolbar.vue'
+
+
 export default {
-  data() {
-    return {
-      itensMenu: [
-        {titulo: 'Início', icon: 'mdi-view-dashboard'},
-        {titulo: 'Sobre', icon: 'mdi-forum'}
-      ],
-      fotos: [
-        {src: 'https://cdn.vuetifyjs.com/images/carousel/squirrel.jpg'},
-        {src: 'https://cdn.vuetifyjs.com/images/carousel/sky.jpg'},
-        {src: 'https://cdn.vuetifyjs.com/images/carousel/bird.jpg'},
-        {src: 'https://cdn.vuetifyjs.com/images/carousel/planet.jpg'}
-      ]
-    }
-  }
+  
+  components: {
+    Carrosel,
+    Footer,
+    Bar,
+    List,
+    Toolbar
+  },
+  
+  
 }
 </script>
